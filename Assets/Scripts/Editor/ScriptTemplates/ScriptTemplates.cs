944873b477f50dea2c99d096d6a6b5ab4867f1fb
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-03-09 20:43:07 +0100

    🧑‍💻 Add DOTS script templates

---
+++ Assets/Scripts/Editor/ScriptTemplates/ScriptTemplates.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace WaynGroup.Mgm.Ability.Editor
+{
+    internal class ScriptTemplates
+    {
+        [MenuItem("Assets/Create/DOTS/System&Component&Authoring")]
+        public static void CreateSystemAndComponentAndAuthoring()
+        {
+            string path = EditorUtility.SaveFilePanelInProject("Choose name", "", "cs", "Please select file name to save your script to:");
+            if(!string.IsNullOrEmpty(path)) 
+            {
+                var fileName = Path.GetFileNameWithoutExtension(path);
+
+                ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                    $"Assets/Scripts/Editor/ScriptTemplates/IComponentData.txt",
+                    fileName + ".cs");
+                ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                    $"Assets/Scripts/Editor/ScriptTemplates/UnmanagedSystem.txt",
+                    fileName + "System.cs");
+                ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                    $"Assets/Scripts/Editor/ScriptTemplates/AuthoringComponent.txt",
+                    fileName + "Authoring.cs");
+            }
+        }
+
+        [MenuItem("Assets/Create/DOTS/IAspect")]
+        public static void CreateIAspect()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                $"Assets/Scripts/Editor/ScriptTemplates/IAspect.txt",
+                "IAspect.cs");
+        }
+
+        [MenuItem("Assets/Create/DOTS/Unmanaged System")]
+        public static void CreateUnmanagedSystem()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                $"Assets/Scripts/Editor/ScriptTemplates/UnmanagedSystem.txt",
+                "UnmanagedSystem.cs");
+        }
+
+        [MenuItem("Assets/Create/DOTS/Authoring Component")]
+        public static void CreateAuthoringComponent()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                $"Assets/Scripts/Editor/ScriptTemplates/AuthoringComponent.txt",
+                "AuthoringComponent.cs");
+        }
+
+        [MenuItem("Assets/Create/DOTS/IComponentData")]
+        public static void CreateIComponentData()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                $"Assets/Scripts/Editor/ScriptTemplates/IComponentData.txt",
+                "IComponentData.cs");
+        }
+        [MenuItem("Assets/Create/DOTS/IBufferElementData")]
+        public static void CreateIBufferElementData()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                $"Assets/Scripts/Editor/ScriptTemplates/IBufferElementData.txt",
+                "IBufferElementData.cs");
+        }
+        [MenuItem("Assets/Create/DOTS/Hybrid Component")]
+        public static void CreateHybridComponent()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                $"Assets/Scripts/Editor/ScriptTemplates/HybridComponent.txt",
+                "HybridComponent.cs");
+        }
+
+    }
+}
