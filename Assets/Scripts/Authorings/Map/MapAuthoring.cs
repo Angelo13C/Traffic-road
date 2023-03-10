8c488cb14d10ee5573c773441993631de1b8eefd
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-03-10 09:33:50 +0100

    ✨ Add map tiles config

--- Assets/Scripts/Authorings/Map/MapAuthoring.cs
+++ Assets/Scripts/Authorings/Map/MapAuthoring.cs
@@ -1,5 +1,6 @@
 using Unity.Entities;
 using UnityEngine;
+using Unity.Collections;
 
 public class MapAuthoring : MonoBehaviour
 {
@@ -10,6 +11,17 @@ public class MapAuthoring : MonoBehaviour
 		public GameObject Prefab;
 	}
 	[SerializeField] private TilePrefab[] _tilePrefabs = new TilePrefab[1];
+	
+	[System.Serializable]
+	private struct TileConfig
+	{
+		public Tile Tile;
+		public Tile AllowedNextTiles;
+
+		public int SpawnRateWeight;
+		public int[] ConsecutiveTilesSpawnRateWeight;
+	}
+	[SerializeField] private TileConfig[] _tileConfigs = new TileConfig[1];
 
 	class Baker : Baker<MapAuthoring>
 	{
@@ -30,6 +42,36 @@ public class MapAuthoring : MonoBehaviour
 					Prefab = GetEntity(authoring._tilePrefabs[i].Prefab)
 				};
 			}
+
+			var mapTileConfigs = new MapTileConfigs();
+			using(var builder = new BlobBuilder(Allocator.Temp))
+			{
+				ref var tilesBlob = ref builder.ConstructRoot<MapTileConfigsArray>();
+				
+				var configsBlob = builder.Allocate(ref tilesBlob.Configs, authoring._tileConfigs.Length);
+				for(var i = 0; i < configsBlob.Length; i++)
+				{
+					var authoringTile = authoring._tileConfigs[i];
+					var consecutiveTiles = builder.Allocate(ref configsBlob[i].ConsecutiveTilesSpawnRateWeight, authoringTile.ConsecutiveTilesSpawnRateWeight.Length);
+					var totalWeightsSum = 0;
+					for(var j = 0; j < consecutiveTiles.Length; j++)
+					{
+						consecutiveTiles[j] = authoringTile.ConsecutiveTilesSpawnRateWeight[j];
+						totalWeightsSum += consecutiveTiles[j];
+					}
+
+					ref var mapTileConfig = ref configsBlob[i];
+					mapTileConfig.Tile = authoringTile.Tile;
+					mapTileConfig.AllowedNextTiles = authoringTile.AllowedNextTiles;
+					mapTileConfig.SpawnRateWeight = authoringTile.SpawnRateWeight;
+					mapTileConfig._totalConsecutiveTilesSpawnRateWeights = totalWeightsSum;
+				}
+
+				var blobAsset = builder.CreateBlobAssetReference<MapTileConfigsArray>(Allocator.Persistent);
+			
+				mapTileConfigs.BlobRefeence = blobAsset;
+			}
+			AddComponent(mapTileConfigs);
 		}
 	}
 }
--- Assets/Scripts/Components/Map/Tile.cs
+++ Assets/Scripts/Components/Map/Tile.cs
@@ -1,7 +1,58 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+
 [System.Flags]
 public enum Tile
 {
     Grass = (1 << 0),
     Road = (1 << 1),
     Water = (1 << 2)
+}
+
+public struct MapTileConfigs : IComponentData
+{
+    public BlobAssetReference<MapTileConfigsArray> BlobRefeence;
+}
+
+public struct MapTileConfigsArray
+{
+    public BlobArray<MapTileConfig> Configs;
+}
+
+[BurstCompile]
+public struct MapTileConfig
+{
+    public Tile Tile;
+    public Tile AllowedNextTiles;
+
+    public int SpawnRateWeight;
+    public BlobArray<int> ConsecutiveTilesSpawnRateWeight;
+    [UnityEngine.HideInInspector] public int _totalConsecutiveTilesSpawnRateWeights;
+
+    [BurstCompile]
+    public int GetRandomConsecutiveTilesCount(ref Random rng)
+    {
+        var randomValue = rng.NextInt(_totalConsecutiveTilesSpawnRateWeights);
+        for(var i = 0; i < ConsecutiveTilesSpawnRateWeight.Length; i++)
+        {
+            if(randomValue < ConsecutiveTilesSpawnRateWeight[i])
+                return i + 1;
+
+            randomValue -= ConsecutiveTilesSpawnRateWeight[i];
+        }
+        return -1;
+    }
+
+    [BurstCompile]
+    public Tile GetRandomNextTile(ref Random rng)
+    {
+        var allowedNextTilesCount = math.countbits((int) AllowedNextTiles);
+        var chosenNextTile = (int) AllowedNextTiles;
+        
+        for (int i = rng.NextInt(allowedNextTilesCount); i > 0; i--) {
+            chosenNextTile &= chosenNextTile - 1; // remove the least significant bit
+        }
+        return (Tile) (chosenNextTile & ~(chosenNextTile - 1));
+    }
 }
