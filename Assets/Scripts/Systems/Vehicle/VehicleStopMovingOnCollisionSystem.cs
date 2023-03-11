da2b2e717c2ed7218ef9334b86a9440b420238c4
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-03-11 12:15:12 +0100

    ⚡️ Remove debug log and burst compile collision event

--- Assets/Scripts/Systems/Vehicle/VehicleStopMovingOnCollisionSystem.cs
+++ Assets/Scripts/Systems/Vehicle/VehicleStopMovingOnCollisionSystem.cs
@@ -23,7 +23,7 @@ public partial struct VehicleStopMovingOnCollisionSystem : ISystem
         }.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
     }
 
-    //[BurstCompile]
+    [BurstCompile]
     private struct RemoveComponentCollisionEvents : ICollisionEventsJob
     {
         [ReadOnly] public CollisionWorld CollisionWorld;
@@ -33,8 +33,6 @@ public partial struct VehicleStopMovingOnCollisionSystem : ISystem
 
         public void Execute(CollisionEvent collisionEvent)
         {
-        if((collisionEvent.EntityA.Index != 103 && collisionEvent.EntityA.Index != 108) || (collisionEvent.EntityB.Index != 103 && collisionEvent.EntityB.Index != 108))
-UnityEngine.Debug.Log("A: " + collisionEvent.EntityA + " | B: " + collisionEvent.EntityB);
             if((CollisionWorld.Bodies[collisionEvent.BodyIndexA].CustomTags & DONT_STOP_VEHICLE_MOVER_CUSTOM_TAGS) == 0 && 
                (CollisionWorld.Bodies[collisionEvent.BodyIndexB].CustomTags & DONT_STOP_VEHICLE_MOVER_CUSTOM_TAGS) == 0)
             {
