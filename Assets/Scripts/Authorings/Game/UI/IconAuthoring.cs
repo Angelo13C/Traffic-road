4ae57f56b2bdb6d0ebc7220a1d1a6f2b12dcad60
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-03-21 14:39:57 +0100

    ✨ Add Icon component

---
+++ Assets/Scripts/Authorings/Game/UI/IconAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public class IconAuthoring : MonoBehaviour
+{
+    [SerializeField] private Texture2D _texture;
+    
+    class Baker : Baker<IconAuthoring>
+    {
+        public override void Bake(IconAuthoring authoring)
+        {
+            var icon = new Icon
+            {
+                Texture = authoring._texture
+            };
+
+            AddComponentObject(icon);
+        }
+    }
+}
---
+++ Assets/Scripts/Components/Game/UI/Icon.cs
@@ -0,0 +1,7 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class Icon : IComponentData
+{
+    public Texture2D Texture;
+}
