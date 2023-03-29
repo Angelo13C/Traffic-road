9930289a355b669a7820898545f636f466659a39
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-03-29 09:22:48 +0200

    🚚 Move camera reference in its own file

--- Assets/Scripts/Components/Game/Other/Camera/CameraRays.cs
+++ Assets/Scripts/Components/Game/Other/Camera/CameraRays.cs
@@ -1,15 +1,8 @@
-using Cinemachine;
 using Unity.Entities;
-using Unity.Mathematics;
 using UnityEngine;
 
 public struct CameraRays : IComponentData
 {
     public Ray Mouse;
     public Ray ScreenCenter;
-}
-
-public class CameraReference : IComponentData
-{
-    public Camera Camera;
 }
---
+++ Assets/Scripts/Components/Game/Other/Camera/CameraReference.cs
@@ -0,0 +1,8 @@
+﻿using Cinemachine;
+using Unity.Entities;
+using UnityEngine;
+
+public class CameraReference : IComponentData
+{
+    public Camera Camera;
+}
