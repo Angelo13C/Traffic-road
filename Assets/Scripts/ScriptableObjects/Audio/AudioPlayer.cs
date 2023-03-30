a2161879ff5e82df1356672696b9e9f11fda262a
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-03-30 20:56:31 +0200

    ✨ Add audio player

---
+++ Assets/Scripts/ScriptableObjects/Audio/AudioPlayer.cs
@@ -0,0 +1,15 @@
+﻿using FMODUnity;
+using Unity.Mathematics;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Audio Player", menuName = "Scriptable Objects/Audio Player")]
+public class AudioPlayer : ScriptableObject
+{
+    [field: SerializeField] public EventReference HornSound { get; private set; }
+    [field: SerializeField] public EventReference CarEngineSound { get; private set; }
+
+    public void PlaySingle(EventReference audio, float3 audioPosition)
+    {
+        RuntimeManager.PlayOneShot(audio, audioPosition);
+    }
+}
