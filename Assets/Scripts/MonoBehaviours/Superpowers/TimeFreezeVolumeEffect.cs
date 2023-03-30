900544d9bdf911b4f4a4f2c0e957a4ddebc00422
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-03-30 09:38:34 +0200

    💄 Improve time freeze volume effect

--- Assets/Scripts/MonoBehaviours/Superpowers/TimeFreezeVolumeEffect.cs
+++ Assets/Scripts/MonoBehaviours/Superpowers/TimeFreezeVolumeEffect.cs
@@ -1,11 +1,18 @@
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Volume))]
 public class TimeFreezeVolumeEffect : MonoBehaviour
 {
-	[SerializeField] [Range(0, 1)] private float _fadeTime = 0.2f;
+	[Header("Fade time")]
+	[SerializeField] [Range(0, 1)] private float _startFadeTime = 0.2f;
+	[SerializeField] [Range(0, 1)] private float _endFadeTime = 0.2f;
+	
+	[Header("Pulse")]
+	[SerializeField] [Range(0, 0.5f)] private float _pulseStrength = 0.1f;
+	[SerializeField] [Range(0, 10)] private float _pulseSpeed = 3f;
 
 	private Volume _timeFreezeVolume;
 	private EntityQuery _timeFreezeSPQuery;
@@ -20,9 +27,19 @@ public class TimeFreezeVolumeEffect : MonoBehaviour
 	private void Update()
 	{
 		var isTimeFrozen = !_timeFreezeSPQuery.IsEmpty;
-		var deltaWeight = Time.deltaTime / _fadeTime;
-		if(!isTimeFrozen)
-			deltaWeight *= -1;
-		_timeFreezeVolume.weight = Mathf.Clamp(_timeFreezeVolume.weight + deltaWeight, 0f, 1f);
+		if (isTimeFrozen)
+		{
+			if (_timeFreezeVolume.weight >= 1 - _pulseStrength)
+			{
+				var weightOffset = math.abs(_pulseStrength * math.sin(Time.time * _pulseSpeed));
+				SetWeight(1 - weightOffset);
+			}
+			else
+				SetWeight(_timeFreezeVolume.weight + Time.deltaTime / _startFadeTime);
+		}
+		else
+			SetWeight(_timeFreezeVolume.weight - Time.deltaTime / _endFadeTime);
 	}
+	
+	private void SetWeight(float value) => _timeFreezeVolume.weight = Mathf.Clamp(value, 0f, 1f);
 }
