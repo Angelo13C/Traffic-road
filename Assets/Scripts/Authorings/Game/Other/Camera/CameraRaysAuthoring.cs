43a32e593f90aefefda89706c314d2d401b39cd3
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-04-01 16:18:17 +0200

    🐛 Fix CameraRays not being able to access Camera.main in the baker
    You can't access Camera.main in the baker, so I moved it in the system

--- Assets/Scripts/Authorings/Game/Other/Camera/CameraRaysAuthoring.cs
+++ Assets/Scripts/Authorings/Game/Other/Camera/CameraRaysAuthoring.cs
@@ -7,17 +7,7 @@ public class CameraRayAuthoring : MonoBehaviour
 	{
 		public override void Bake(CameraRayAuthoring authoring)
 		{
-			var cameraReference = new CameraReference
-			{
-				Camera = Camera.main
-			};
-			AddComponentObject(cameraReference);
-			
-			var cameraRays = new CameraRays {
-				Mouse = cameraReference.Camera.ScreenPointToRay(Input.mousePosition),
-				ScreenCenter =  cameraReference.Camera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2))
-			};
-			AddComponent(cameraRays);
+			AddComponent<CameraRays>();
 		}
 	}
 }
--- Assets/Scripts/Systems/Game/Other/Camera/UpdateCameraRaysSystem.cs
+++ Assets/Scripts/Systems/Game/Other/Camera/UpdateCameraRaysSystem.cs
@@ -1,4 +1,4 @@
-using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 
@@ -6,6 +6,17 @@ public partial struct UpdateCameraRaysSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
+        var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
+        foreach (var cameraReferenceEntity in SystemAPI.QueryBuilder().WithAll<CameraRays>().WithNone<CameraReference>()
+                     .Build().ToEntityArray(Allocator.Temp))
+        {
+            entityCommandBuffer.AddComponent(cameraReferenceEntity, new CameraReference
+            {
+                Camera = Camera.main
+            });
+        }
+        entityCommandBuffer.Playback(state.EntityManager);
+        
         foreach (var (cameraReference, cameraRays) in SystemAPI.Query<CameraReference, RefRW<CameraRays>>())
         {
             cameraRays.ValueRW.Mouse = cameraReference.Camera.ScreenPointToRay(Input.mousePosition);
