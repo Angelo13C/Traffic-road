80ce3bd1228cdc8425fe534c75b4c59410ad371c
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-04-02 11:53:49 +0200

    🐛 Fix change drag on events authoring not properly serializing values

--- Assets/Scripts/Authorings/Game/Other/ChangeDragOnEventsAuthoring.cs
+++ Assets/Scripts/Authorings/Game/Other/ChangeDragOnEventsAuthoring.cs
@@ -1,8 +1,5 @@
 using Unity.Entities;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public class ChangeDragOnEventsAuthoring : MonoBehaviour
 {
@@ -12,44 +9,20 @@ public class ChangeDragOnEventsAuthoring : MonoBehaviour
 		public bool Change;
 		public float NewValue;
 	}
-	[HideInInspector] public Event CollisionEvent;
-	[HideInInspector] public Event HitByExplosionEvent;
-	[HideInInspector] public Event BlackHoleEvent;
+	[SerializeField] private Event _collisionEvent;
+	[SerializeField] private Event _hitByExplosionEvent;
+	[SerializeField] private Event _blackHoleEvent;
 
 	class Baker : Baker<ChangeDragOnEventsAuthoring>
 	{
 		public override void Bake(ChangeDragOnEventsAuthoring authoring)
 		{
-			if(authoring.CollisionEvent.Change)
-				AddComponent(new ChangeDragOnCollision { NewDrag = authoring.CollisionEvent.NewValue });
-			if(authoring.HitByExplosionEvent.Change)
-				AddComponent(new ChangeDragOnHitByExplosion { NewDrag = authoring.HitByExplosionEvent.NewValue });
-			if(authoring.BlackHoleEvent.Change)
-				AddComponent(new ChangeDragOnBlackHole { NewDrag = authoring.BlackHoleEvent.NewValue });
+			if(authoring._collisionEvent.Change)
+				AddComponent(new ChangeDragOnCollision { NewDrag = authoring._collisionEvent.NewValue });
+			if(authoring._hitByExplosionEvent.Change)
+				AddComponent(new ChangeDragOnHitByExplosion { NewDrag = authoring._hitByExplosionEvent.NewValue });
+			if(authoring._blackHoleEvent.Change)
+				AddComponent(new ChangeDragOnBlackHole { NewDrag = authoring._blackHoleEvent.NewValue });
 		}
 	}
 }
-
-#if UNITY_EDITOR
-[CustomEditor(typeof(ChangeDragOnEventsAuthoring))]
-public class ChangeDragOnEventsAuthoringEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-
-        var authoring = target as ChangeDragOnEventsAuthoring;
-		DrawEvent("Change on collision", authoring.CollisionEvent);
-		DrawEvent("Change on hit by explosion", authoring.HitByExplosionEvent);
-		DrawEvent("Change on black hole", authoring.BlackHoleEvent);
-    }
-
-	private void DrawEvent(string changeName, ChangeDragOnEventsAuthoring.Event eventToDraw)
-	{		
-        eventToDraw.Change = EditorGUILayout.Toggle(changeName, eventToDraw.Change);
-
-        if (eventToDraw.Change)
-            eventToDraw.NewValue = EditorGUILayout.FloatField("New linear drag", eventToDraw.NewValue);
-	}
-}
-#endif
